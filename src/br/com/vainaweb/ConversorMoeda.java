2b5a9d640751f56df4fefcaeea4ecf7156554a55
JuniorD-Isael <isaeldjunior@gmail.com>
2023-12-05 07:50:54 -0300

    ConversorMoedas BR/US concluido

--- src/br/com/vainaweb/ConversorMoeda.java
+++ src/br/com/vainaweb/ConversorMoeda.java
@@ -1,4 +1,5 @@
 package br.com.vainaweb;
+
 import java.util.Scanner;
 
 public class ConversorMoeda {
@@ -8,11 +9,18 @@ public class ConversorMoeda {
         int opcao = 0;
         double cotacaoDolar = 4.89;
 
+
         do {
             exibirOpcoesConversor();
-            exibirOpcoesConversorUS();
             double valor, resultado;
 
+            if (sc.hasNextInt()) {
+                opcao = sc.nextInt();
+            } else {
+                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
+                sc.next();
+                continue;
+            }
             switch (opcao) {
                 case 1:
                     System.out.println("Digite o valor em dólar: ");
@@ -33,14 +41,6 @@ public class ConversorMoeda {
                     System.out.println("Opção Inválida!");
                     exibirOpcoesConversor();
             }
-            if (sc.hasNextInt()) {
-                opcao = sc.nextInt();
-            } else {
-                System.out.println("Entrada inválida. Por favor, digite um número inteiro.");
-                sc.next();
-                continue;
-            }
-
         } while (opcao != 3);
         sc.close();
     }
@@ -49,25 +49,25 @@ public class ConversorMoeda {
         System.out.println("Escolha uma opção de conversão: ");
         System.out.println(" [1] Dólar para Real" + "\n [2] Real para Dólar" + "\n [3] Voltar");
     }
-    
+
     public static void mainUS() {
         Scanner sc = new Scanner(System.in);
         int opcao = 0;
-        float cotacaoDolar = 4.89f; 
+        double cotacaoDolar = 4.89;
 
         do {
             exibirOpcoesConversorUS();
-            float valor, resultado;
+            double valor, resultado;
 
             switch (opcao) {
                 case 1:
-                	System.out.println("Enter the value in dollars: ");
+                    System.out.println("Enter the value in dollars: ");
                     valor = sc.nextFloat();
                     resultado = valor / cotacaoDolar;
                     System.out.println("The result is: R$" + resultado);
                     break;
                 case 2:
-                	System.out.println("Enter the value in Brazilian real: ");
+                    System.out.println("Enter the value in Brazilian real: ");
                     valor = sc.nextFloat();
                     resultado = valor * cotacaoDolar;
                     System.out.println("The result is: US$" + resultado);
@@ -79,23 +79,21 @@ public class ConversorMoeda {
                     System.err.println("Invalid Option!");
                     exibirOpcoesConversorUS();
             }
-            
+
             if (sc.hasNextInt()) {
                 opcao = sc.nextInt();
             } else {
-            	System.out.println("Invalid input. Please enter an integer.");
+                System.out.println("Invalid input. Please enter an integer.");
                 sc.next();
                 continue;
             }
-
         } while (opcao != 3);
         sc.close();
     }
 
     public static void exibirOpcoesConversorUS() {
-    	System.out.println("Choose a conversion option: ");
-    	System.out.println(" [1] Dollar to Real" + "\n [2] Real to Dollar" + "\n [3] Back");
+        System.out.println("Choose a conversion option: ");
+        System.out.println(" [1] Dollar to Real" + "\n [2] Real to Dollar" + "\n [3] Back");
 
     }
-    
 }
